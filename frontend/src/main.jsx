81ecd97624776813aac0e8a0bc34b15a9e27ef8a
Elton Paa Kwesi Sakyi <eltonpaasakyi@gmail.com>
2026-01-16 23:58:32 -0500

    Deliverable 1: Frontend environment setup and animated splash screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className="text-4xl font-bold text-blue-600">
-      Tailwind v4 is working
-    </h1>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
